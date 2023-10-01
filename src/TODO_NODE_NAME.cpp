32053b890ab16c05d108e18670e128d39af34cf5
Andrew Ealovega <Andrew@Ealovega.dev>
2023-10-01 17:56:44 -0400

    Make it actually compile

--- src/TODO_NODE_NAME.cpp
+++ src/TODO_NODE_NAME.cpp
@@ -1,13 +1,16 @@
-#include <cstdio>
-
-#include "TODO_NODE_NAME/TODO_NODE_NAME_node.hpp"
+#include "TODO_PACKAGE_NAME/TODO_NODE_NAME_node.hpp"
 
 int main(int argc, char** argv) {
+    // Setup runtime
     rclcpp::init(argc, argv);
     rclcpp::executors::MultiThreadedExecutor exec;
     rclcpp::NodeOptions options;
+
+    // Add nodes to executor
     auto node = std::make_shared<TODO_NODE_NAME>(options);
     exec.add_node(node);
+
+    // Run
     exec.spin();
     rclcpp::shutdown();
     return 0;
--- src/TODO_NODE_NAME_node.cpp
+++ src/TODO_NODE_NAME_node.cpp
@@ -1,12 +1,16 @@
 #include "TODO_PACKAGE_NAME/TODO_NODE_NAME_node.hpp"
 
+// For _1
+using namespace std::placeholders;
+
 TODO_NODE_NAME::TODO_NODE_NAME(const rclcpp::NodeOptions& options) : Node("TODO_NODE_NAME", options) {
     // Parameters
     float x = this->declare_parameter<float>("foo", -10.0);
 
     // Pub Sub
-    this->sub = this->create_subscription<std_msgs::msg::String>("/str", std::bind(&TODO_NODE_NAME::sub_cb, this, _1));
-    this->path_pub = this->create_publisher<std_msgs::msg::String>("/run_folder");
+    this->sub =
+        this->create_subscription<std_msgs::msg::String>("/str", 1, std::bind(&TODO_NODE_NAME::sub_cb, this, _1));
+    this->pub = this->create_publisher<std_msgs::msg::String>("/run_folder", 1);
 
     // Log a sample log
     RCLCPP_INFO(this->get_logger(), "You passed %f", x);
@@ -14,10 +18,10 @@ TODO_NODE_NAME::TODO_NODE_NAME(const rclcpp::NodeOptions& options) : Node("TODO_
     // Send a sample message
     std_msgs::msg::String msg{};
     msg.data = std::string{"Hello World!"};
-    path_pub->publish(msg);
+    pub->publish(msg);
 }
 
 void TODO_NODE_NAME::sub_cb(const std_msgs::msg::String::SharedPtr msg) {
     // Echo message
-    this->pub.publish(msg);
+    this->pub->publish(*msg);
 }
